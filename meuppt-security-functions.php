90292e5cf9ab4dc85f50b20682c5b7792e5f37ad
Carlos Matos <carlos@startupeando.com.br>
2017-10-26 19:34:53 +0100

    Update meuppt-security-functions.php

--- meuppt-security-functions.php
+++ meuppt-security-functions.php
@@ -20,6 +20,26 @@ Possui uma série de melhorias de segurança para instalações em Wordpress. Di
 
 **************************************************************************/
 
+// Aciona autoupdater a partir do Github
+
+include_once('updater.php');
+
+if (is_admin()) { 
+		$config = array(
+			'slug' => plugin_basename(__FILE__),
+			'proper_folder_name' => 'meuppt-security-functions', // this is the name of the folder your plugin lives in
+			'api_url' => 'https://api.github.com/repos/lipsworld/meuppt-security-functions', // the GitHub API url of your GitHub repo
+			'raw_url' => 'https://raw.github.com/lipsworld/meuppt-security-functions/master', // the GitHub raw url of your GitHub repo
+			'github_url' => 'https://github.com/lipsworld/meuppt-security-functions', // the GitHub url of your GitHub repo
+			'zip_url' => 'https://github.com/lipsworld/meuppt-security-functions/zipball/master', // the zip url of the GitHub repo
+			'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+			'requires' => '3.0', // which version of WordPress does your plugin require?
+			'tested' => '4.8', // which version of WordPress is your plugin tested up to?
+			'readme' => 'README.md', // which file to use as the readme for the version number
+			'access_token' => '', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
+		);
+		new WP_GitHub_Updater($config);
+	}
 
 
 // Impede exibição de versão do WP em META e em RSS
