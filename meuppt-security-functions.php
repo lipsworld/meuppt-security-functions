dfede637539c423ff1a227e603d635af5b7106d9
Carlos Matos <carlos@startupeando.com.br>
2017-11-03 17:34:17 +0000

    Update meuppt-security-functions.php

--- meuppt-security-functions.php
+++ meuppt-security-functions.php
@@ -51,7 +51,7 @@ if (is_admin()) {
 			'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
 			'requires' => '3.0', // which version of WordPress does your plugin require?
 			'tested' => '4.8.3', // which version of WordPress is your plugin tested up to?
-			'readme' => 'README.md', // which file to use as the readme for the version number
+			'readme' => 'readme.txt', // which file to use as the readme for the version number
 			'access_token' => '', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
 		);
 		new WP_GitHub_Updater($config);
