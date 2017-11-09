e1ecbb05e9289db7c0655e18c0aff763f34a828f
Carlos Matos <carlos@startupeando.com.br>
2017-11-09 10:09:05 +0000

    Fix do bloqueio a emojis
    Economia de 2.3 kb no carregamento do cabeçalho

--- meuppt-security-functions.php
+++ meuppt-security-functions.php
@@ -97,16 +97,17 @@ remove_action('wp_head', 'rsd_link');
 
 // Desabilita emojis de um modo geral
 
-function disable_wp_emojicons() {
-  add_filter( 'tiny_mce_plugins', 'disable_emojicons_tinymce');
-  remove_action('admin_print_styles', 'print_emoji_styles');
-  remove_action('wp_head', 'print_emoji_detection_script', 1);
-  remove_action('admin_print_scripts', 'print_emoji_detection_script');
-  remove_action('wp_print_styles', 'print_emoji_styles');
-  remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
-  remove_filter('the_content_feed', 'wp_staticize_emoji');
-  remove_filter('comment_text_rss', 'wp_staticize_emoji'); 
+function meuppt_disable_emojis() {
+ remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+ remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+ remove_action( 'wp_print_styles', 'print_emoji_styles' );
+ remove_action( 'admin_print_styles', 'print_emoji_styles' ); 
+ remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+ remove_filter( 'comment_text_rss', 'wp_staticize_emoji' ); 
+ remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+ add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
 }
+add_action( 'init', 'meuppt_disable_emojis' );
 
 function disable_emojicons_tinymce( $plugins ) {
   if (is_array($plugins)) {
